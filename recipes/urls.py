5e47e651f17002dfa90595a49ed7dd1efe98c8f0
adradan <ericvsolcruz@gmail.com>
2023-10-27 11:24:31 -0400

    Adding a user + switch to use request.user

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/", views.group)
+    path("group/", views.group),
+    path("group/add", views.add_user_to_group)
 ]
