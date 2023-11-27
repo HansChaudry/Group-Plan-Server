4da2462fcff741f508d603f5aeef3b37065f01f2
Eric Vianney Sol Cruz <ericvsolcruz@gmail.com>
2023-11-26 20:20:39 -0500

    Eric/group polls (#27)
    * Polls (wip)
    
    * updated model name of votes and poll recipes
    
    ---------
    
    Co-authored-by: Hans Chaudry <hanschaudry@gmail.com>

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Recipe, RecipeGroup
+from .models import Recipe, RecipeGroup, Vote, PollRecipe
 
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(RecipeGroup)
+admin.site.register(Vote)
+admin.site.register(PollRecipe)
