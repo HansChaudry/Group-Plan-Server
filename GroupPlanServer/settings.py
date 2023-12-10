87aaab1f465a02ed4fd9189b88303afbe6c951a0
Hans Chaudry <62669434+HansChaudry@users.noreply.github.com>
2023-12-09 19:12:33 -0500

    initial deployment (#33)

--- GroupPlanServer/settings.py
+++ GroupPlanServer/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ur7xh!2x4c3!)r7ivr)o04*_qbtl$l&v4ugx($wo-ft5@a)z82'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', '192.168.1.51', '192.168.1.209']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', 'GroupPlan.pythonanywhere.com']
 
 load_dotenv()
 
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
